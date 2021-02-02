456a86241142685277f393fea4bd983f473d8f9b
jekoo <pjkwprn@gmail.com>
2021-02-02 23:48:51 +0900

    feat : id button 컴포넌트 구축 styled components, material ui 적용

---
+++ front/black-gomushin/src/components/LoginView/Input/id.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+
+const idInput = () => {
+  return (
+    <>
+      <TextField id="outlined-search" label="id" variant="outlined" />
+    </>
+  );
+};
+
+export default idInput;
--- front/black-gomushin/src/pages/LoginView.jsx
+++ front/black-gomushin/src/pages/LoginView.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
+import IdInput from '../components/LoginView/Input/id';
+import { StylesProvider } from '@material-ui/core';
+
+const IdInputBox = styled(IdInput)`
+  background-color: red;
+`;
+
+const Container = styled.div`
+  display: flex;
+`;
 
 const LoginView = () => {
   return (
-    <>
+    <Container>
       <span>로그인 페이지</span>
-    </>
+      <StylesProvider injectFirst>
+        <IdInputBox></IdInputBox>
+      </StylesProvider>
+    </Container>
   );
 };
 
