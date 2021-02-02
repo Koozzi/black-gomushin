d6b814a851213763f5447fd0ce2cb6bc837b4ffe
jekoo <pjkwprn@gmail.com>
2021-02-02 22:40:40 +0900

    feat : router 설정 로그인, 서비스 라우팅 구분 react-router-dom 설치

--- front/black-gomushin/src/App.js
+++ front/black-gomushin/src/App.js
@@ -1,24 +1,26 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
 function App() {
+  const loginRouter = (
+    <>
+      <Route path="/login" component={LoginView} />
+      <Redirect from="*" to="/login" />
+    </>
+  );
+
+  const serviceRouter = (
+    <>
+      <Switch>
+        <Route path="">hello</Route>
+      </Switch>
+    </>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>{loginRouter}</BrowserRouter>
+    </>
   );
 }
 export default App;
