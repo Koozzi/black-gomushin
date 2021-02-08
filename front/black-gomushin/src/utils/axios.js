f8248d0e7fb0976d4c7882109c0b92278d46ad92
jekoo <pjkwprn@gmail.com>
2021-02-08 21:10:04 +0900

    feat : axios default setting header 에 토큰값 스토어에서 받아서 사용하도록 처리

--- front/black-gomushin/src/utils/axios.js
+++ front/black-gomushin/src/utils/axios.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
+import store from '../store/index';
 
+const AUTH_TOKEN = store.getState().user.token;
 axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.common['Authorization'] = AUTH_TOKEN || null;
 
 export const getAxios = (reqUrl) =>
   axios
