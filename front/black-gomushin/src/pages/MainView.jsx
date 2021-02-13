dfb1d489dabb0b6673e0972a6e6f2e29912460ba
jekoo <pjkwprn@gmail.com>
2021-02-14 00:42:39 +0900

    feat : axios default header setHeader 함수로 초기 토큰값 인스턴스에 저장

--- front/black-gomushin/src/pages/MainView.jsx
+++ front/black-gomushin/src/pages/MainView.jsx
@@ -1,29 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import adImg from '../assets/img/sample-black.jpg';
 import Header from '../components/common/header';
+import { getAxios } from '../utils/axios';
+
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex-direction: cloumn;
+  justify-content: center;
+`;
+
+const AdContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 10%;
+  background-color: #e3ecf1;
+  height: 100%;
+`;
+
+const AD = styled.img`
+  width: 40%;
+  height: 60%;
+`;
 
 const MainView = () => {
-  const Container = styled.div`
-    display: flex;
-    width: 100%;
-    height: 100%;
-    flex-direction: cloumn;
-    justify-content: center;
-  `;
-
-  const AdContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    margin-top: 10%;
-    background-color: #e3ecf1;
-    height: 100%;
-  `;
-
-  const AD = styled.img`
-    width: 40%;
-    height: 60%;
-  `;
+  const [data, setData] = useState({ items: [] });
+  useEffect(() => {
+    const getItems = async () => {
+      const result = await getAxios('items/');
+      console.log(result);
+      // setData(result.data);
+    };
+    getItems();
+  }, []);
 
   return (
     <>
