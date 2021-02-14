e5b96c688b39a5761e68d45c22a53be140875cc4
jekoo <pjkwprn@gmail.com>
2021-02-14 16:25:06 +0900

    feat : footer 컴포넌트 구현 개발자 정보, 개발 기간, 메인 설명 컴포넌트 구축

--- front/black-gomushin/src/components/common/footer.jsx
+++ front/black-gomushin/src/components/common/footer.jsx
@@ -17,21 +17,60 @@ const MainInfo = styled.div`
   align-items: center;
 `;
 
+const MoveSiteBox = styled.h5`
+  &:hover {
+    color: orange;
+    cursor: pointer;
+  }
+`;
+
+const DevelopInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const Developer = styled.h5`
+  &:hover {
+    cursor: pointer;
+    color: orange;
+  }
+`;
+
+const BaseInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const moveSite = () => {
+  window.location.href = 'https://github.com/Koozzi/black-gomushin';
+};
+
+const getGitHubSite = (userName) => {
+  window.location.href = `https://github.com/${userName}`;
+};
+
 const Footer = () => {
-  const moveSite = () => {
-    window.location.href = 'https://github.com/Koozzi/black-gomushin';
-  };
   return (
     <FooterContainer>
       <MainInfo>
         <h5>신발 중고 거래 플랫폼</h5>
         <h3> 👟 검정고무신 </h3>
-        <h3 onClick={moveSite}> 깃허브 구경가기 👈</h3>
+        <MoveSiteBox onClick={moveSite}> 깃허브 구경가기 👈</MoveSiteBox>
       </MainInfo>
-      <div>
-        <div>Developers</div>
-        <div>Production Period</div>
-      </div>
+      <DevelopInfo>
+        <h3>Developers</h3>
+        <Developer onClick={() => getGitHubSite('Zigje9')}>👨‍💻 FE-Zigje9</Developer>
+        <Developer onClick={() => getGitHubSite('Koozzi')}>👨‍🎓 BE-Koozzi</Developer>
+        <Developer onClick={() => getGitHubSite('JaeHeee')}>🤵‍♂️ Mobile-JaeHeee</Developer>
+        <Developer onClick={() => getGitHubSite('Namukk')}>🕵️‍♂️ Admin-Namukk</Developer>
+      </DevelopInfo>
+      <BaseInfo>
+        <h6>develop period: 2021.01.30 ~ 2021.03.14</h6>
+      </BaseInfo>
     </FooterContainer>
   );
 };
