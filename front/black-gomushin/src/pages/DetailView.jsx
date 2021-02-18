d0af481082e6689bfee67b4787515e2ffd3ecc8a
jekoo <pjkwprn@gmail.com>
2021-02-18 20:55:24 +0900

    feat : detailView info item 정보 레이아웃 설정

--- front/black-gomushin/src/pages/DetailView.jsx
+++ front/black-gomushin/src/pages/DetailView.jsx
@@ -6,28 +6,43 @@ import Footer from '../components/common/footer';
 const DetailContainer = styled.div`
   display: flex;
   width: 100%;
-  height: 100%;
+  height: 60%;
   flex-direction: column;
   justify-content: center;
+  align-items: center;
+`;
+
+const ItemImg = styled.img`
+  display: flex;
+  max-height: 40vh;
+  max-width: 40vh;
+  padding-top: 5%;
 `;
 
 const DetailItem = styled.div`
   display: flex;
-  justify-content: center;
-  margin-top: 10%;
+  flex-direction: column;
+  align-items: center;
   background-color: #e3ecf1;
-  height: 30%;
+  height: 40vh;
 `;
 
 const DetailView = ({ location: { state } }) => {
   const item = state.item;
-  console.log(item.title);
   return (
     <>
       <Header></Header>
       <DetailContainer>
+        <ItemImg src={item.imageurl}></ItemImg>
         <DetailItem>
           <div>{`${item.title}`}</div>
+          <div>{`${item.state}`}</div>
+          <div>{`${item.price}원`}</div>
+          <div>{`${item.content}`}</div>
+          <div>{`판매자${item.sell_username}`}</div>
+          <div>{`조회수 ${item.view}`}</div>
+          <div>{`사이즈 ${item.size}`}</div>
+          <div>{`${item.publish_date}`}</div>
         </DetailItem>
       </DetailContainer>
       <Footer></Footer>
