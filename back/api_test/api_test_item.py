b8088465c0c3b115709a345651875b19c3888051
koozzi <koozzi666@gmail.com>
2021-03-05 13:58:40 +0900

    feat : Test Code

---
+++ back/api_test/api_test_item.py
@@ -0,0 +1,39 @@
+import requests
+import time
+import os
+
+TOKEN = os.environ['KOOZZI_TOKEN']
+
+def test_get_search_item_by_keyword(keyword):
+    start_time = time.time()
+    print("=============================================")
+    print("Search API Test by Keyword [Keyword : {0}]".format(keyword), end=" ")
+    endpoint = "http://3.34.127.185:8000/search/?offset=0&limit=5&size=260&keyword={0}".format(keyword)
+    headers = {"Authorization": "Bearer {0}".format(TOKEN)}
+    response = requests.get(endpoint, headers=headers)
+    
+    if response.status_code != 200:
+        print("..... Error")
+        print(response.text)
+        exit()
+    print("..... Good")
+    print("Test execution time : ", time.time() - start_time)
+
+
+def test_get_item_detail_by_id(item_id):
+    start_time = time.time()
+    print("=============================================")
+    print("Item Detail Test by Id [Item Id : {0}]".format(item_id), end=" ")
+    endpoint = "http://3.34.127.185:8000/item/{0}".format(item_id)
+    headers = {"Authorization": "Bearer {0}".format(TOKEN)}
+    response = requests.get(endpoint, headers=headers)
+    
+    if response.status_code != 200:
+        print("..... Error")
+        print(response.text)
+        exit()
+    print("..... Good")
+    print("Test execution time : ", time.time() - start_time)
+
+test_get_search_item_by_keyword('jordan')
+test_get_item_detail_by_id(3855)
---
+++ back/api_test/api_test_user.py
@@ -0,0 +1,39 @@
+import requests
+import time
+import os
+
+TOKEN = os.environ['KOOZZI_TOKEN']
+
+def test_get_user_detail_by_id(user_id):
+    start_time = time.time()
+    print("=============================================")
+    print("User Detail Test by Id [User Id : {0}]".format(user_id), end=" ")
+    endpoint = "http://3.34.127.185:8000/user/?id={0}".format(user_id)
+    headers = {"Authorization": "Bearer {0}".format(TOKEN)}
+    response = requests.get(endpoint, headers=headers)
+    
+    if response.status_code != 200:
+        print("..... Error")
+        print(response.text)
+        exit()
+    print("..... Good")
+    print("Test execution time : ", time.time() - start_time)
+
+
+def test_get_user_detail_by_token():
+    start_time = time.time()
+    print("=============================================")
+    print("User Detail Test by Token", end=" ")
+    endpoint = "http://3.34.127.185:8000/profile"
+    headers = {"Authorization": "Bearer {0}".format(TOKEN)}
+    response = requests.get(endpoint, headers=headers)
+    
+    if response.status_code != 200:
+        print("..... Error")
+        print(response.text)
+        exit()
+    print("..... Good")
+    print("Test execution time : ", time.time() - start_time)
+
+test_get_user_detail_by_id(2)
+test_get_user_detail_by_token()
